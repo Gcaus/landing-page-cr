88a8de50079352f6f61d5dda42efd3d313764fea
Gabriel Caus <gabriel.caus@codenapp.com>
2024-08-06 20:45:01 -0300

    ajustes

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ document.addEventListener("DOMContentLoaded", () => {
     header.classList.toggle("scrolled", window.scrollY > 50);
   });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
