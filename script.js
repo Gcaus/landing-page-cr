988159d702b5d82ec3974ce4f909b33d512cdf7d
Gabriel Caus <gabriel.caus@codenapp.com>
2024-08-07 18:17:28 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.getElementById("menu-toggle");
   const menu = document.getElementById("menu");
+  const menuItems = document.querySelectorAll(".menu li a");
 
   menuToggle.addEventListener("click", () => {
     menu.classList.toggle("show");
+  });
+
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      menu.classList.remove("show");
+    });
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!menu.contains(e.target) && !menuToggle.contains(e.target)) {
+      menu.classList.remove("show");
+    }
   });
 
   // Adiciona efeito de scroll no header
